f4e62125fe85f358b2ca4f89debf4ec7c91dfcc4
DuSeifert <eduardoseifertes@gmail.com>
2024-09-12 14:37:37 -0300

    fizz buzz

---
+++ Atividade_3/src/main/java/org/example/FizzBuzz.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class FizzBuzz {
+
+    int n;
+
+    public FizzBuzz(int n){
+        this.n = n;
+    }
+
+
+    public void executar(){
+        int i = 1;
+
+        while(i<=n){
+
+
+        }
+    }
+
+}
